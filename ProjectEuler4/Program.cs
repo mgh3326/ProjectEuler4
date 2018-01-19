35de1d63010c5ce67eb483885e655bb6e4247a7e
mgh3326 <mgh3326@naver.com>
2018-01-19 23:16:35 +0900

    complete

--- ProjectEuler4/Program.cs
+++ ProjectEuler4/Program.cs
@@ -2,11 +2,29 @@
 
 namespace ProjectEuler4
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var max = 0;
+            for (var i = 100; i < 1000; i++)
+            {
+                for (var j = 100; j < 1000; j++)
+                {
+                    var str = (i * j).ToString();
+                    for (var k = 0; k < str.Length / 2; k++)
+                    {
+                        if (str[k] != str[str.Length - 1 - k])
+                            break;
+                        if (str[k] != str[str.Length - 1 - k]) continue;
+                        if (k != str.Length / 2 - 1) continue;
+                        if (max < Convert.ToInt32(str))
+                            max = Convert.ToInt32(str);
+                        //Console.WriteLine(str);
+                    }
+                }
+            }
+            Console.WriteLine("max : " + max);
         }
     }
 }
